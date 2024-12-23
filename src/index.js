c59089ba475e59422ef6a13f7d92596f7a0d2574
plunkydev <plunkydesign@gmail.com>
2024-12-23 17:32:06 -0500

    Se integró navegación dinámica y funciones de scroll: importación del componente 'nav' y funciones 'scrollNavEfect' y 'scrollToId', creación dinámica de la barra de navegación con 'navMain', configuración de evento 'scroll' para mostrar/ocultar la barra, y evento de clic en 'navPerfilContiner' para desplazarse al inicio. Mantiene la funcionalidad de renderizado de artículos al cargar la página.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import { articles } from './arcticles'
 import { renderArticles } from './displayArticlesCards'
+import { nav, scrollNavEfect, scrollToId } from './navigation/nav.js'
 import './styles.css'
+const navContainer = document.getElementById('navMain')
+navContainer.appendChild(nav)
+
+const home = document.getElementById('heroContiner')
+const btnToPerfil = document.getElementById('navPerfilContiner')
+document.addEventListener('scroll', () => scrollNavEfect(navContainer))
+btnToPerfil.addEventListener('click', () => scrollToId(home))
 
 document.addEventListener('DOMContentLoaded', () => {
   renderArticles(articles)
