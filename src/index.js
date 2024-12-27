a78a2bb5f759441a967b723fe23e628c51f9005a
plunkydev <plunkydesign@gmail.com>
2024-12-27 12:06:45 -0500

    refactor: Integrar funciones de menú, agregar eventos para navegación y mejorar interacciones con botones.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,34 @@
 import { articles } from './arcticles'
 import { renderArticles } from './displayArticlesCards'
 import { nav, scrollNavEfect, scrollToId } from './navigation/nav.js'
+import { showMenu, closeMenu } from './utils/menu.js'
 import './styles.css'
+
 const navContainer = document.getElementById('navMain')
 navContainer.appendChild(nav)
 
 const home = document.getElementById('heroContiner')
+const acercaDeMiContiner = document.getElementById('acercaDeMiContiner')
+const ArticulosDestacados = document.getElementById('featuredArticlesContiner')
 const btnToPerfil = document.getElementById('navPerfilContiner')
-document.addEventListener('scroll', () => scrollNavEfect(navContainer))
+
+export const btn = document.getElementById('imgMenuHamburgesa')
+export const img = document.getElementById('imgMenuHamburgesa')
+export const menuList = document.getElementById('menuList')
+
+const btnHome = document.getElementById('btnHome')
+const btnacercaDeMiContiner = document.getElementById('btnacercaDeMiContiner')
+const btnArticulosDestacados = document.getElementById('btnArticulosDestacados')
+
 btnToPerfil.addEventListener('click', () => scrollToId(home))
+btn.addEventListener('mouseover', showMenu)
+btn.addEventListener('click', closeMenu)
+btnHome.addEventListener('click', () => scrollToId(home))
+btnacercaDeMiContiner.addEventListener('click', () => scrollToId(acercaDeMiContiner))
+btnArticulosDestacados.addEventListener('click', () => scrollToId(ArticulosDestacados))
 
 document.addEventListener('DOMContentLoaded', () => {
   renderArticles(articles)
 })
+document.addEventListener('scroll', () => scrollNavEfect(navContainer))
+document.addEventListener('click', () => closeMenu())
