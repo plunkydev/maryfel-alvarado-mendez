213647d57ead1e68186aec9c36a7dc06080ca561
plunkydev <plunkydesign@gmail.com>
2025-02-28 15:06:41 -0500

    Update articles and template files

--- src/arcticles.js
+++ src/arcticles.js
@@ -1,56 +1,45 @@
+import img1 from './assets/imgs-articulos/club-de-escritura-para-ninos.webp'
+import img2 from './assets/imgs-articulos/escritura-creativa-para-jovenes.webp'
+import img3 from './assets/imgs-articulos/netelip-5-estrategias-presencia-online.webp'
+import img4 from './assets/imgs-articulos/crea-un-minicuento.webp'
+import img5 from './assets/imgs-articulos/Escuelas-innovadoras-luca.webp'
+import img6 from './assets/imgs-articulos/habilidades_docentes-luca.webp'
+
 export const articles = [
-  {
-    title: 'Gestión Inteligente: 4 Herramientas para Modernizar los Procesos de tu Empresa',
-    description: 'Emplear herramientas para modernizar los procesos de tu empresa se traduce en una mayor productividad, eficiencia y rendimiento empresarial. Por sus',
-    image: 'https://www.elcontribuyente.mx/wp-content/uploads/2024/01/empresarios.jpg',
-    url: 'https://www.elcontribuyente.mx/2024/01/gestion-inteligente-4-herramientas-para-modernizar-los-procesos-de-tu-empresa/'
-  },
   {
     title: 'Club de Escritura para Niños: Fomenta su Imaginación',
     description: 'Te contamos cómo puedes crear un club de escritura para niños que fomente su libertad creativa y autonomía: ayúdalos a pensar por sí mismos',
-    image: 'https://ejerciciosdeescrituracreativa.com/wp-content/uploads/2024/05/Club-de-escritura-para-ninos.jpg',
+    image: `${img1}`,
     url: 'https://ejerciciosdeescrituracreativa.com/club-de-escritura-para-ninos/'
   },
   {
     title: '¿Cómo Fomentar la Escritura Creativa para Jóvenes?',
     description: 'En este artículo te contamos cuáles son los beneficios de la escritura creativa en los jóvenes y qué actvidades pueden implementarse para fomentarla.',
-    image: 'https://ejerciciosdeescrituracreativa.com/wp-content/uploads/2024/03/Escritura-creativa-para-jovenes.jpg',
+    image: `${img2}`,
     url: 'https://ejerciciosdeescrituracreativa.com/escritura-creativa-para-adolescentes/'
   },
   {
     title: 'Eleva tu presencia online: 5 estrategias para impulsar la comunicación digital',
     description: 'Eleva tu presencia online: 5 estrategias para impulsar la comunicación digital',
-    image: 'https://www.netelip.com/blog/wp-content/uploads/2024/02/netelip-5-estrategias-presencia-online.jpg',
+    image: `${img3}`,
     url: 'https://www.netelip.com/blog/2024/04/eleva-tu-presencia-online-5-estrategias-para-impulsar-la-comunicacion-digital/'
   },
   {
     title: 'Crea un Minicuento',
     description: 'En este ejercicio de escritura creativa te invitamos a escribir un minicuento, basado en una imagen mientras el tiempo corre ¡Tic tac! ¡Tic tac!',
-    image: 'https://ejerciciosdeescrituracreativa.com/wp-content/uploads/2024/06/Crea-un-minicuento.jpg',
+    image: `${img4}`,
     url: 'https://ejerciciosdeescrituracreativa.com/crea-un-minicuento/'
-  },
-  {
-    title: 'Tipos de negocios online más exitosos en 2024 - TyN Magazine',
-    description: 'Te contamos cuáles son los tipos de negocios online más exitosos para este año 2024 para que puedas escoger una opción que se adapte a tus necesidades.',
-    image: 'https://tynmagazine.com/wp-content/uploads/sites/3/2024/02/ecommerce-3415340_1280.jpg',
-    url: 'https://tynmagazine.com/tipos-de-negocios-online-mas-exitosos-en-2024/'
-  },
-  {
-    title: 'Optimiza tu Negocio: 6 Estrategias de Email Marketing en 2024 | Brand Bakers 📈',
-    description: 'Potencia tu empresa en 2024 con las últimas Estrategias de Email Marketing. Descubre tácticas innovadoras\n    para mejorar la comunicación, aumentar la participación y obtener conversiones impactantes. Eleva tu\n    presencia digital ahora.',
-    image: 'https://brandbackers.com.mx/assets/img/blog/como-aumentar-trafico-web/slide-blog-trafico-web.jpg',
-    url: 'https://brandbackers.com.mx/blog-de-marketing-digital/optimiza-tu-negocio-6-estrategias-de-email-marketing-en-2024'
   },
   {
     title: '¿Qué son las escuelas innovadoras? - Luca',
     description: 'En este artículo se expone qué son las escuelas innovadoras y de qué manera contribuyen a la transformación de la educación en México',
-    image: 'https://www.lucaedu.com/wp-content/uploads/2023/01/Escuelas-innovadoras.jpg',
+    image: `${img5}`,
     url: 'https://www.lucaedu.com/escuelas-innovadoras/'
   },
   {
     title: '9 habilidades docentes - Luca',
     description: 'En este artículo se explicará qué son las habilidades docentes y cuáles son las indispensables que hay que aplicar en el aula',
-    image: 'https://www.lucaedu.com/wp-content/uploads/2022/12/habilidades_docentes.jpg',
+    image: `${img6}`,
     url: 'https://www.lucaedu.com/habilidades-docentes/'
   }
 ]
--- src/displayArticlesCards.js
+++ src/displayArticlesCards.js
@@ -1,29 +1,15 @@
-import logo from './assets/logoMaryfel.png'
-
-export function validateImage (url) {
-  return new Promise((resolve, reject) => {
-    // eslint-disable-next-line no-undef
-    const img = new Image()
-    img.onload = () => resolve(true) // Imagen válida
-    img.onerror = () => resolve(false) // Imagen inválida
-    img.src = url
-  })
-}
-
 export async function renderArticles (articles) {
   const container = document.getElementById('articlesContainer')
   container.innerHTML = ''
 
   for (const article of articles) {
-    const isValid = await validateImage(article.image)
-
     const card = document.createElement('div')
     card.classList.add('article-card')
 
     card.innerHTML = `
             <div class="card-content">
                 <a href="${article.url}" target="_blank" class="card-link">
-                    <img src="${isValid ? article.image : logo}" 
+                    <img src="${article.image}" 
                         alt="${article.title}" 
                         class="card-image" />
                     <h2 class="card-title">${article.title}</h2>
